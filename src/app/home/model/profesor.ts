27bfbeaf6ccf67afee05d7d0940f41ff1d047a8a
Diego Cabrera <d.cabrera@duocuc.cl>
2023-09-03 11:16:21 -0300

    modelo profesor

--- src/app/home/model/profesor.ts
+++ src/app/home/model/profesor.ts
@@ -1,14 +1,11 @@
-export class Alumno {
+export class Profesor {
     constructor(
         public rut: string,
         public nombre: string,
         public apellido: string,
-        public fechaNacimiento: Date,
-        public carrera: string,
         public correoElectronico: string,
-        public horario: string,
         public asignatura: string,
+        public horario: string,
         public contrasena: string
     ) {}
 }
-
