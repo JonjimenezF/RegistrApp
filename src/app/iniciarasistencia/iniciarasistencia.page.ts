9537f163fc5bc4c73babd8627a2d5e9440180b76
Vina del Mar <vina@duoc.acad>
2023-11-25 09:22:36 -0300

    error

--- src/app/iniciarasistencia/iniciarasistencia.page.ts
+++ src/app/iniciarasistencia/iniciarasistencia.page.ts
@@ -12,6 +12,7 @@ import { IonicModule } from '@ionic/angular';
 })
 export class IniciarasistenciaPage implements OnInit {
 
+  // apidata: any = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={\"fecha\":\"2023-11-25\",\"hora\":\"22:00\",\"estado_asistencia\":\"Ausente\",\"id_seccion\":\"1\",\"rut_alumno\":\"12345678-1\",\"rut_profesor\":\"11243145-1\"}"
   constructor() { }
 
   ngOnInit() {
